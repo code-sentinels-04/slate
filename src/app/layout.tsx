fc318318ab095382102b559bd9d960c6a787f7d5
GauravDhanraja <gauravdhanraja88@proton.me>
2024-11-23 18:59:23 +0530

    Created t3 app with google login

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+
+export const metadata: Metadata = {
+  title: "Slate",
+  description: "Sign Language to Test Exchange",
+  icons: [{ rel: "icon", url: "/slate.svg" }],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <SessionProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>{children}</body>
+      </html>
+    </SessionProvider>
+  );
+}
